95a1643af99e325d6e4a420ee41e9d83a7e12b3e
hienviluong125 <hienviluong125@gmail.com>
2021-05-23 19:57:19 +0700

    add play again logic

--- public/index.js
+++ public/index.js
@@ -2,22 +2,32 @@ $(document).ready(function () {
     const socket = io();
     let isHost = false;
     let yourTurn = false;
+    let canPlay = false;
+    let roomToken = "";
 
     // get uuid from server
     socket.on('user_joined', data => {
         $('#token').val(data.token);
     })
 
+    socket.on('joined_room', data => {
+        roomToken = data.roomToken;
+        $('.start_game').prop("disabled", false);
+        $('#status-label').html("Your joined room succesfully. Press Start game to start");
+    })
+
     // be a host
     socket.on('be_a_host', data => {
         isHost = true;
         yourTurn = false;
+        yourTurn ? $('#status-label').html("Your turn") : $('#status-label').html("Rival's turn");
     })
 
     // be a guest
     socket.on('be_a_guest', data => {
         isHost = false;
         yourTurn = true;
+        yourTurn ? $('#status-label').html("Your turn") : $('#status-label').html("Rival's turn");
     })
 
 
@@ -26,16 +36,50 @@ $(document).ready(function () {
         yourTurn = !yourTurn;
         let btnId = data.id;
 
+        yourTurn ? $('#status-label').html("Your turn") : $('#status-label').html("Rival's turn");
         $('#' + btnId).prop('disabled', true);
 
-        if(!isHost) {
-            $('#' + btnId).css({color: 'blue'});
+        if (!isHost) {
+            $('#' + btnId).css({ color: 'blue' });
             $('#' + btnId).html('X');
         } else {
-            $('#' + btnId).css({color: 'red'})
+            $('#' + btnId).css({ color: 'red' })
             $('#' + btnId).html('O');
         }
+    })
 
+    socket.on('wait_for_rival', data => {
+        $('#status-label').html("Waiting for rival");
+    })
+
+    socket.on("tie", data => {
+        $('.caro-btn').prop('disabled', true)
+        $('.start_game').prop('disabled', true)
+        $('.reset_game').prop('disabled', false)
+        $('#status-label').html("TIE")
+        $('#status-label').css("color", "yellow");
+    })
+
+    socket.on("win", data => {
+        $('.caro-btn').prop('disabled', true)
+        $('.start_game').prop('disabled', true)
+        $('.reset_game').prop('disabled', false)
+        $('#status-label').html("You WIN")
+        $('#status-label').css("color", "green");
+        for (let pos of data.positions) {
+            $('#' + pos.toString()).css({ background: 'yellow' })
+        }
+    })
+
+    socket.on("lose", data => {
+        $('.caro-btn').prop('disabled', true)
+        $('.start_game').prop('disabled', true)
+        $('.reset_game').prop('disabled', false)
+        $('#status-label').html("Your LOSE")
+        $('#status-label').css("color", "red");
+        for (let pos of data.positions) {
+            $('#' + pos.toString()).css({ background: 'yellow' })
+        }
     })
 
     // join room action
@@ -48,22 +92,37 @@ $(document).ready(function () {
     })
 
     // Hit caro action
-    $('.caro-btn').on('click', function() {
-        if(!yourTurn) {
+    $('.caro-btn').on('click', function () {
+        if (!yourTurn) {
             return;
         }
 
-        if(isHost) {
-            $(this).css({color: 'blue'});
+        if (isHost) {
+            $(this).css({ color: 'blue' });
             $(this).html('X');
         } else {
-            $(this).css({color: 'red'})
+            $(this).css({ color: 'red' })
             $(this).html('O');
         }
 
         yourTurn = !yourTurn;
+        yourTurn ? $('#status-label').html("Your turn") : $('#status-label').html("Rival's turn");
         $(this).prop('disabled', true);
-        socket.emit("hit_board", { id: $(this).attr('id') });
+        socket.emit("hit_board", { id: $(this).attr('id'), isHost: isHost });
+    })
+
+    $('.start_game').on('click', function() {
+        socket.emit("start_game", { token: $('#token').val(), roomToken: roomToken });
+        $(this).prop('disabled', true)
+    })
+
+    $('.reset_game').on('click', function() {
+        $('.start_game').prop('disabled', true)
+        $('.caro-btn').css({'background': 'gray'})
+        $('.caro-btn').prop('disabled', false)
+        $('.caro-btn').html('')
+        $('#status-label').css('color', 'black');
+        socket.emit("start_game", { token: $('#token').val(), roomToken: roomToken });
     })
 
 })
--- server.js
+++ server.js
@@ -14,6 +14,15 @@ server.listen(port, () => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 let users = {};
+let matrix = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+let rooms = {};
+let hostJoined = false;
+let guestJoined = false;
+let roomToken = "";
 
 io.on('connection', (socket) => {
     const yourId = uuidv4();
@@ -25,12 +34,64 @@ io.on('connection', (socket) => {
     socket.emit('user_joined', { token: yourId });
 
     socket.on('join_by_token', data => {
-        users[data.token].emit("be_a_host");
-        socket.emit("be_a_guest");
+        roomToken = data.token;
+        hostJoined = false;
+        guestJoined = false;
+        matrix = resetMatrix();
+
+        socket.join(data.token);
+        users[data.token].join(data.token);
+
+        io.to(roomToken).emit("joined_room", { roomToken: roomToken });
+    })
+
+    socket.on('start_game', data => {
+        if (data.roomToken === data.token) {
+            hostJoined = true
+        } else {
+            guestJoined = true
+        }
+
+        if (hostJoined === true && guestJoined === true) {
+            if (data.roomToken === data.token) {
+                socket.emit("be_a_host")
+                socket.broadcast.emit("be_a_guest")
+            } else {
+                socket.emit("be_a_guest")
+                socket.broadcast.emit("be_a_host")
+            }
+        } else {
+            socket.emit("wait_for_rival");
+        }
     })
 
     socket.on('hit_board', data => {
         socket.broadcast.emit('receive_hit_board', data);
+
+        // check is Win
+        let label = data.isHost ? 1 : 2;
+        let pos = parseInt(data.id);
+        let row = parseInt(pos / 3);
+        let col = pos % 3;
+
+        matrix[row][col] = label;
+        let result = checkWin(data.isHost);
+
+        if (result.isWin) {
+            socket.emit("win", { positions: result.positions })
+            socket.broadcast.emit("lose", { positions: result.positions })
+            hostJoined = false;
+            guestJoined = false;
+            matrix = resetMatrix();
+        } else {
+            if (checkTie()) {
+                socket.emit("tie")
+                socket.broadcast.emit("tie")
+                hostJoined = false;
+                guestJoined = false;
+                matrix = resetMatrix();
+            }
+        }
     });
 
     socket.on('user_quit', data => {
@@ -39,8 +100,8 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', data => {
-        for(let userId in users) {
-            if(users[userId].id == socket.id) {
+        for (let userId in users) {
+            if (users[userId].id == socket.id) {
                 console.log("user: ", userId, " quitted")
                 delete users[userId];
                 console.log("FROM DISCONNECT current_users: ", Object.keys(users));
@@ -49,6 +110,102 @@ io.on('connection', (socket) => {
         }
     })
 });
+
+function checkWin(isHost) {
+    return checkWinFor(isHost);
+}
+
+function checkTie() {
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (matrix[row][col] === 0) {
+                return false
+            }
+        }
+    }
+
+    return true
+}
+
+function checkWinFor(isHost) {
+    let label = isHost ? 1 : 2;
+    if (matrix[0][0] === label && matrix[0][0] === matrix[0][1] && matrix[0][0] === matrix[0][2]) {
+        return {
+            positions: [0, 1, 2],
+            isWin: true,
+            isHost: isHost
+        }
+    }
+
+    if (matrix[0][0] === label && matrix[0][0] === matrix[1][1] && matrix[0][0] === matrix[2][2]) {
+        return {
+            positions: [0, 4, 8],
+            isWin: true,
+            isHost: isHost
+        }
+    }
+
+    if (matrix[0][0] === label && matrix[0][0] === matrix[1][0] && matrix[0][0] === matrix[2][0]) {
+        return {
+            positions: [0, 3, 6],
+            isWin: true,
+            isHost: isHost
+        }
+    }
+
+    if (matrix[1][0] === label && matrix[1][0] === matrix[1][1] && matrix[1][0] === matrix[1][2]) {
+        return {
+            positions: [3, 4, 5],
+            isWin: true,
+            isHost: isHost
+        }
+    }
+
+    if (matrix[2][0] === label && matrix[2][0] === matrix[2][1] && matrix[2][0] === matrix[2][2]) {
+        return {
+            positions: [6, 7, 8],
+            isWin: true,
+            isHost: isHost
+        }
+    }
+
+    if (matrix[2][0] === label && matrix[2][0] === matrix[1][1] && matrix[2][0] === matrix[0][2]) {
+        return {
+            positions: [6, 4, 2],
+            isWin: true,
+            isHost: isHost
+        }
+    }
+
+    if (matrix[0][1] === label && matrix[0][1] === matrix[1][1] && matrix[0][1] === matrix[2][1]) {
+        return {
+            positions: [1, 4, 7],
+            isWin: true,
+            isHost: isHost
+        }
+    }
+
+    if (matrix[0][2] === label && matrix[0][2] === matrix[1][2] && matrix[0][2] === matrix[2][2]) {
+        return {
+            positions: [2, 5, 8],
+            isWin: true,
+            isHost: isHost
+        }
+    }
+
+    return {
+        isWin: false,
+        isHost: isHost
+    }
+}
+
+function resetMatrix() {
+    return [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
+}
 
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
